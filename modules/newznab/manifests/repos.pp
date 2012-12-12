54d96915ea5630109c9f00b503248abd60d6c8e4
Colin Panisset <github@clabber.com>
2012-12-12 17:48:58 +1100

    Use different method of fetching rpmfusion release packages

--- modules/newznab/manifests/repos.pp
+++ modules/newznab/manifests/repos.pp
@@ -1,12 +1,31 @@
 class newznab::repos {
-  # We can install rpmfusion repos via yum directly from the repo itself
+  # the yum provider for package doesn't do localinstall, so we have to
+  # fetch the RPMs for rpmfusion first. The other option is to specify the
+  # repos explicitly using the 'yumrepo' type
+
+  $rpmfusion_free_pkg = 'rpmfusion-free-release-6-1.noarch.rpm'
+  $rpmfusion_nonfree_pkg = 'rpmfusion-nonfree-release-6-1.noarch.rpm'
+
+  exec { 'fetch-rpmfusion-free-rpm':
+    creates => "/var/tmp/$rpmfusion_free_pkg",
+    command => "/usr/bin/curl -o /var/tmp/$rpmfusion_free_pkg http://download1.rpmfusion.org/free/el/updates/6/$architecture/$rpmfusion_free_pkg"
+  }
+
   package { 'rpmfusion-free-release':
-    ensure => installed,
-    source => "http://download1.rpmfusion.org/free/el/updates/6/$architecture/rpmfusion-free-release-6-1.noarch.rpm",
+    ensure      => installed,
+    source      => "/var/tmp/$rpmfusion_free_pkg",
+    require     => Exec['fetch-rpmfusion-free-rpm']
+  }
+
+  exec { 'fetch-rpmfusion-nonfree-rpm':
+    creates => "/var/tmp/$rpmfusion_nonfree_pkg",
+    command => "/usr/bin/curl -o /var/tmp/$rpmfusion_nonfree_pkg http://download1.rpmfusion.org/nonfree/el/updates/6/$architecture/$rpmfusion_nonfree_pkg"
   }
 
   package { 'rpmfusion-nonfree-release':
-    ensure => installed,
-    source => "http://download1.rpmfusion.org/nonfree/el/updates/6/$architecture/rpmfusion-nonfree-release-6-1.noarch.rpm"
+    ensure      => installed,
+    source      => "/var/tmp/$rpmfusion_nonfree_pkg",
+    require     => Exec['fetch-rpmfusion-nonfree-rpm']
   }
+
 }
